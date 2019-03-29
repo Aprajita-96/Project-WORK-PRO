00d03bee028fa59344337530d96d4aae1a9c75f2
roopali <roopaligandhi900@gmail.com>
2019-03-29 18:00:52 +0530

    productOwner dashboard

--- workpro-frontend/src/app/app-routing.module.ts
+++ workpro-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProjectOwnerDashboardComponent} from './project-owner-dashboard/project-owner-dashboard.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'project-owner-dashboard', component: ProjectOwnerDashboardComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- workpro-frontend/src/app/app.module.ts
+++ workpro-frontend/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProjectOwnerDashboardComponent } from './project-owner-dashboard/project-owner-dashboard.component';
+import { UsercardProjectOwnerDashboardComponent } from './usercard-project-owner-dashboard/usercard-project-owner-dashboard.component';
+import {HttpClientModule} from'@angular/common/http';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectOwnerDashboardComponent,
+    UsercardProjectOwnerDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

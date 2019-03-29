42ad2137f24c4eb29c121207f2f3e810ae586ed0
apbajpa1 <aprajithabajpai@gmail.com>
2019-03-29 17:17:11 +0530

    login

--- workpro-frontend/src/app/app-routing.module.ts
+++ workpro-frontend/src/app/app-routing.module.ts
@@ -1,35 +1,41 @@
-import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import {ProjectCardUserDashboardComponent} from './project-card-user-dashboard/project-card-user-dashboard.component';
-import {  ProjectDetailsComponent } from './project-details/project-details.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { FreelancerdetailsComponent } from './freelancerdetails/freelancerdetails.component';
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { UserDashboardComponent } from "./user-dashboard/user-dashboard.component";
+import { ProjectCardUserDashboardComponent } from "./project-card-user-dashboard/project-card-user-dashboard.component";
+import { ProjectDetailsComponent } from "./project-details/project-details.component";
+import { RegisterComponent } from "./register/register.component";
+import { LoginComponent } from "./login/login.component";
+import { FreelancerdetailsComponent } from "./freelancerdetails/freelancerdetails.component";
 
 const routes: Routes = [
   {
-    path: "project", component: ProjectDetailsComponent
+    path: "project",
+    component: ProjectDetailsComponent
   },
   {
-    path:"register", component: RegisterComponent
+    path: "register",
+    component: RegisterComponent
   },
   {
-    path:"login" , component: LoginComponent
+    path: "login",
+    component: LoginComponent
   },
   {
-    path: 'userdashboard', component: UserDashboardComponent
+    path: "userdashboard",
+    component: UserDashboardComponent
   },
   {
-  path:'podashboard' , component:ProjectCardUserDashboardComponent
-  } ,
+    path: "podashboard",
+    component: ProjectCardUserDashboardComponent
+  },
   {
-  path:"freelancerdetails", component:FreelancerdetailsComponent
-}
+    path: "freelancerdetails",
+    component: FreelancerdetailsComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

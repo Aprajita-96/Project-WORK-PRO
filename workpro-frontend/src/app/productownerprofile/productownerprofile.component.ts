515c7fc68220363b07b222bcec338cce3de51885
apbajpa1 <aprajithabajpai@gmail.com>
2019-04-01 13:14:52 +0530

    poDetails

--- workpro-frontend/src/app/productownerprofile/productownerprofile.component.ts
+++ workpro-frontend/src/app/productownerprofile/productownerprofile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FreelancerDetailsService} from '../freelancer-details.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-productownerprofile',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./productownerprofile.component.scss']
 })
 export class ProductownerprofileComponent implements OnInit {
-
-  constructor() { }
+  productowner:any={};
+  constructor(private po:FreelancerDetailsService, private router:Router) { }
 
   ngOnInit() {
+  }
+  save(productowner){
+    console.log(productowner);
+    console.log("user details entered")
+    this.po.setDetailsofProductOwner(productowner).subscribe(console.log);
+    this.router.navigateByUrl('podashboard')
   }
 
 }

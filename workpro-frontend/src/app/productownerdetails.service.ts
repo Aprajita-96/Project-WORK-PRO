1d7254e672bb28cf53c8dfe554d676a47c29d041
sharma12344 <pratima447@gmail.com>
2019-04-09 15:58:36 +0530

    bidview

--- workpro-frontend/src/app/productownerdetails.service.ts
+++ workpro-frontend/src/app/productownerdetails.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductownerdetailsService {
-
-  constructor() { }
+  value:any;
+  data:any;
+  constructor(private http:HttpClient) { }
+  getDtailsOfProjectOwner(value){
+    return this.http.get("http://localhost:8083/api/v1/projectOwner/"+value);
+  }
+  updateDetailsOfProjectOwner(value,data)
+  {
+    return this.http.put("http://localhost:8083/api/v1/projectOwner/"+value,data);
+  }
 }

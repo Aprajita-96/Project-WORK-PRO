5c58a9be57469f22b8a76a31b55edc0013bbb3ea
AnuragRatnaIBM <anuragratna00@gmail.com>
2019-04-09 16:20:10 +0530

    kafka

--- freelancer-profile-service/src/main/java/com/stackroute/freelancerprofile/service/BidKafkaService.java
+++ freelancer-profile-service/src/main/java/com/stackroute/freelancerprofile/service/BidKafkaService.java
@@ -7,6 +7,9 @@ import com.stackroute.freelancerprofile.repository.BidKafkaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class BidKafkaService {
     private BidKafkaRepository bidKafkaRepository;
@@ -18,5 +21,23 @@ public class BidKafkaService {
     public BidKafka save(BidKafka bid) {
         BidKafka result = bidKafkaRepository.save(bid);
         return result;
+    }
+    public List<BidKafka> search(String email) {
+        List<BidKafka> resultFreelancer = new ArrayList<BidKafka>();
+
+        List<BidKafka> list = bidKafkaRepository.findAll();
+        for (BidKafka skill1 : list) {
+            System.out.println("inside For");
+            System.out.println(skill1);
+            if ((skill1.getFreelancerEmail()).equals(email)) {
+//                LOGGER.info("inside if............................." + email);
+//                LOGGER.info(skill1.getFreelancerEmail());
+                resultFreelancer.add(skill1);
+                System.out.println(resultFreelancer);
+            }
+        }
+        return resultFreelancer;
+
+
     }
 }

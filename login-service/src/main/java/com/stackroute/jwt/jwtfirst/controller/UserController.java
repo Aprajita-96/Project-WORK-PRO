d0f57d0e76c4894264c81ab85c582baeaea854bd
Annuusha <nayakanusha90@gmail.com>
2019-04-15 10:02:51 +0530

    modifed

--- login-service/src/main/java/com/stackroute/jwt/jwtfirst/controller/UserController.java
+++ login-service/src/main/java/com/stackroute/jwt/jwtfirst/controller/UserController.java
@@ -9,12 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 @CrossOrigin("*")
 public class UserController {
 

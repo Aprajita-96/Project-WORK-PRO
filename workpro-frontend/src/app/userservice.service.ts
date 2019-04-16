06ad103c9681620c3befc93227800f6ff67f783c
aprasa32 <aprasa32@in.ibm.com>
2019-04-16 11:51:27 +0530

    ADDED ZULL API

--- workpro-frontend/src/app/userservice.service.ts
+++ workpro-frontend/src/app/userservice.service.ts
@@ -13,7 +13,7 @@ export class UserserviceService {
   }
   loginUser(user: any): Observable<any> {
     const headers = new HttpHeaders({ "Access-Control-Allow-Orgin": "*" });
-    return this.http.post("http://13.234.155.75:8080/login/login", user, {
+    return this.http.post("http://13.234.155.75:8080/login/api/v1/login", user, {
       headers: headers
     });
   }
